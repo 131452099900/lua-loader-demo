2bb053b2e4d6e2b78614f30eedb3c58cb37106e8
gbl <gbl403687@gmail.com>
2025-03-11 15:18:00 +0800

    新增测试

--- src/main/java/com/Sp.java
+++ src/main/java/com/Sp.java
@@ -1,6 +1,7 @@
 package com;
 
 import com.luamapper.LuaMapper1;
+import lua.xgwd.me.core.LuaMapperFactory;
 import lua.xgwd.me.core.mapper.A;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -14,8 +15,13 @@ import org.springframework.context.ConfigurableApplicationContext;
 public class Sp {
     public static void main(String[] args) {
         ConfigurableApplicationContext run = SpringApplication.run(Sp.class, args);
-        LuaMapper1 bean = run.getBean(LuaMapper1.class);
-        System.out.println(bean.luaScpt(new String[]{"k1", "k2"}, new String[]{"v1", "v2"}));
+        // 通过直接获取接口代理注入
+        LuaMapper1 luaMapper1 = run.getBean(LuaMapper1.class);
+        System.out.println(luaMapper1.luaScpt(new String[]{"k1", "k2"}, new String[]{"v1", "v2"}));
 
+        // 通过工厂注入
+        LuaMapperFactory luaMapperFactory = run.getBean(LuaMapperFactory.class);
+        LuaMapper1 mapper = luaMapperFactory.getMapper(LuaMapper1.class);
+        Long aLong = mapper.luaScpt(new String[]{"k1", "k2"}, new String[]{"v1", "v2"});
     }
 }
